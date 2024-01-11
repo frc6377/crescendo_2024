6d132453f53ac6651ed02813624b1d2a6c40b09a
zaneb <zanebrotherton@gmail.com>
2024-01-10 21:06:47 -0600

    Apply spotless

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -9,7 +9,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LimelightHelpers;
-
 import java.util.function.BiConsumer;
 
 public class LimelightSubsystem extends SubsystemBase {
