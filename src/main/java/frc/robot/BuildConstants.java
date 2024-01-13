1e40cd694bc94b8a8bb481ae53ffef5e7ff2ccaa
modboyex <modboyex@gmail.com>
2024-01-13 12:21:22 -0600

    Add Build Constants to git for proper building

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "crescendo_2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "dc9c358de413b2c1bcfb6b0e575358363083c91d";
+  public static final String GIT_DATE = "2024-01-10 20:55:47 CST";
+  public static final String GIT_BRANCH = "Install-Advantage-Kit";
+  public static final String BUILD_DATE = "2024-01-13 12:19:57 CST";
+  public static final long BUILD_UNIX_TIME = 1705169997232L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
