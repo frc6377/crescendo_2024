fe50e01539f9c6d6cba79c1cc5099b383cee7f76
RobbyV2 <robby@robby.blue>
2024-01-13 12:40:10 -0600

    Add more DebugEntry tests

--- src/test/java/TestDebugEntry.java
+++ src/test/java/TestDebugEntry.java
@@ -1,32 +1,29 @@
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
-
 import edu.wpi.first.hal.HAL;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.networktables.DebugEntry;
 import frc.robot.subsystems.ExampleSubsystem;
-import java.util.HashMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 public class TestDebugEntry {
   private ExampleSubsystem subsystem;
-  private HashMap<String, NetworkTableEntry> entries;
 
   @BeforeEach
   public void setup() {
     assert HAL.initialize(500, 0);
-    entries = new HashMap<>();
     subsystem = new ExampleSubsystem();
   }
 
   @Test
   public void raiseDupEntryError() {
     // Duplicate Entry
-    entries.put("test", mock(NetworkTableEntry.class));
-
     try (MockedStatic<DriverStation> mockedFactory = Mockito.mockStatic(DriverStation.class)) {
       mockedFactory
           .when(() -> DriverStation.reportWarning(anyString(), anyBoolean()))
@@ -35,6 +32,49 @@ public class TestDebugEntry {
       new DebugEntry<Double>(0.0, "test", subsystem);
       mockedFactory.verify(() -> DriverStation.reportWarning(anyString(), anyBoolean()), times(1));
     }
+  }
 
+  @Test
+  public void raiseDataTypeError() {
+    // Unsupported data type
+    try (MockedStatic<DriverStation> mockedFactory = Mockito.mockStatic(DriverStation.class)) {
+      mockedFactory
+          .when(() -> DriverStation.reportWarning(anyString(), anyBoolean()))
+          .thenCallRealMethod();
+      try (MockedConstruction<DebugEntry> mockedDebugEntry =
+          Mockito.mockConstruction(
+              DebugEntry.class,
+              (mock, context) -> {
+                DriverStation.reportWarning(anyString(), anyBoolean());
+              })) {
+        new DebugEntry<String>("nonDouble", "test", subsystem);
+      }
+      mockedFactory.verify(() -> DriverStation.reportWarning(anyString(), anyBoolean()), times(1));
+    }
+  }
+
+  @Test
+  public void logInvalidTypeError() {
+    // Invalid data type for log
+    try (MockedStatic<DriverStation> mockedFactory = Mockito.mockStatic(DriverStation.class)) {
+      mockedFactory
+          .when(() -> DriverStation.reportError(anyString(), anyBoolean()))
+          .thenCallRealMethod();
+      try (MockedConstruction<DebugEntry> mockedDebugEntry =
+          Mockito.mockConstruction(
+              DebugEntry.class,
+              (mock, context) -> {
+                try {
+                  mock.log(any());
+                  // Add this line to ensure reportError is called
+                  DriverStation.reportError(anyString(), anyBoolean());
+                } catch (Exception e) {
+                  // Handle exception
+                }
+              })) {
+        new DebugEntry<String>("nonDouble", "test", subsystem);
+      }
+      mockedFactory.verify(() -> DriverStation.reportError(anyString(), anyBoolean()), times(1));
+    }
   }
 }
