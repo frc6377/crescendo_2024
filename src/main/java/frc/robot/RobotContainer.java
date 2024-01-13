3e451e07d40e37eea33da146d3ff2fab53820ca6
MalachiLewis <mdl@jdlmx.com>
2024-01-13 14:23:35 -0600

    Spotless

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,8 +19,8 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.IntakeCommand;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.color.SignalingSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.color.SignalingSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
