1e0c8bd5cd8378c37dbbda4ad0d0051014e9483f
KianChilds <94756266+KianChilds@users.noreply.github.com>
2024-01-13 12:11:46 -0600

    Spotless Apply

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,23 +4,20 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class IntakeSubsystem extends SubsystemBase {
 
   private CANSparkMax intakeMotor;
 
-
   public IntakeSubsystem() {
     intakeMotor = new CANSparkMax(Constants.IntakeConstants.INTAKE_MOTOR_ID, MotorType.kBrushless);
     intakeMotor.restoreFactoryDefaults();
     intakeMotor.setSmartCurrentLimit(40);
-
   }
 
   public Command intakeCommand() {
@@ -37,8 +34,7 @@ public class IntakeSubsystem extends SubsystemBase {
   }
 
   @Override
-  public void periodic() {
-  }
+  public void periodic() {}
 
   @Override
   public void simulationPeriodic() {
