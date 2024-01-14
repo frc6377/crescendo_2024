63c840855c63c94fa0c4cc328ea9e0d166162f3e
KianChilds <94756266+KianChilds@users.noreply.github.com>
2024-01-14 15:34:05 -0600

    Fixed Merge Error

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,6 +20,7 @@ import frc.robot.commands.IntakeCommand;
 import frc.robot.config.DynamicRobotConfig;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.color.SignalingSubsystem;
 
 /**
@@ -40,7 +41,7 @@ public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final HowdyXboxController m_driverController =
       new HowdyXboxController(OperatorConstants.kDriverControllerPort);
-  private final CommandSwerveDrivetrain drivetrain;
+  private final SwerveSubsystem drivetrain;
 
   private final SignalingSubsystem signalingSubsystem =
       new SignalingSubsystem(1, m_driverController::setRumble);
