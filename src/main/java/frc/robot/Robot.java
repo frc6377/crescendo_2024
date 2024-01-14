e7984d17ced2784a4b906b07b580ea560034b285
KianChilds <94756266+KianChilds@users.noreply.github.com>
2024-01-14 15:21:44 -0600

    Squashed commit of the following:
    commit 67ed20bcf872e4d88a92d358aceb0da6f93d6f6c
    Merge: ac35f76 8711134
    Author: ModBoyex <79735432+ModBoyex@users.noreply.github.com>
    Date:   Sun Jan 14 14:26:52 2024 -0600
    
        Merge pull request #19 from frc6377/Install-Advantage-Kit
    
        Install Advantage Kit
    
    commit 8711134c6dfed7b178c6ae55abd6f2b3beeaf3bd
    Author: modboyex <modboyex@gmail.com>
    Date:   Sun Jan 14 14:23:41 2024 -0600
    
        Conform to naming standards
    
    commit cf89dadfddd8baffa53484de5b09e59d263d7996
    Author: modboyex <modboyex@gmail.com>
    Date:   Sun Jan 14 14:20:34 2024 -0600
    
        Remove ignore build error
    
    commit d893983a95305114684397d954de8454ac96701e
    Author: modboyex <modboyex@gmail.com>
    Date:   Sun Jan 14 14:10:52 2024 -0600
    
        Spotless strikes again (Part: 346)
    
    commit 05f65a541a693642c392fd9ad6522af52cd5e938
    Author: modboyex <modboyex@gmail.com>
    Date:   Sun Jan 14 14:08:24 2024 -0600
    
        add boolean for replay
    
    commit f5687ecd5519f826ad7a667040b969a1ed9ceaa3
    Merge: 44fff02 ac35f76
    Author: JD Lewis <lohengrin332@users.noreply.github.com>
    Date:   Sun Jan 14 13:39:37 2024 -0600
    
        Merge branch 'main' into Install-Advantage-Kit
    
    commit 44fff02c843023f3e377a6e04ec780a740fb6968
    Author: Casey Brotherton <caseyjbrotherton@gmail.com>
    Date:   Sun Jan 14 12:05:34 2024 -0600
    
        remove generated BuildConstants.java
    
    commit b1cd2cd564e7321cb5bbf5485eb5054dc13c7825
    Author: modboyex <modboyex@gmail.com>
    Date:   Sat Jan 13 12:21:47 2024 -0600
    
        Don't track Build Constants
    
    commit 1e40cd694bc94b8a8bb481ae53ffef5e7ff2ccaa
    Author: modboyex <modboyex@gmail.com>
    Date:   Sat Jan 13 12:21:22 2024 -0600
    
        Add Build Constants to git for proper building
    
    commit dc9c358de413b2c1bcfb6b0e575358363083c91d
    Author: modboyex <modboyex@gmail.com>
    Date:   Wed Jan 10 20:55:47 2024 -0600
    
        Recover form Git shinanagins
    
    commit 28aa01095d635b77d11ab29db98ef2be49eccb89
    Author: modboyex <modboyex@gmail.com>
    Date:   Wed Jan 10 20:52:59 2024 -0600
    
        Test
    
    commit 5b45f64cfc28a513d194eef00fa70f39e7950497
    Author: modboyex <modboyex@gmail.com>
    Date:   Wed Jan 10 20:52:58 2024 -0600
    
        Log GIT meta data
    
    commit 1ac0d4e7015d29cb8af727f0dde5b15f830325d3
    Author: modboyex <modboyex@gmail.com>
    Date:   Wed Jan 10 20:52:57 2024 -0600
    
        Remove Build Constants
    
    commit 424861ca25ecdcfdfc59502b595edd29948673bf
    Author: modboyex <modboyex@gmail.com>
    Date:   Wed Jan 10 20:52:56 2024 -0600
    
        Add BuildConstants.java
    
    commit 70b394509571e1e337e0dd7133659353870c6763
    Author: modboyex <modboyex@gmail.com>
    Date:   Wed Jan 10 20:52:55 2024 -0600
    
        Edit Robot.java to allow Advatage Kit to log
    
    commit e11fab4db6c2f26234ac9a6b4be6923f0135eefb
    Author: modboyex <modboyex@gmail.com>
    Date:   Wed Jan 10 20:52:54 2024 -0600
    
        Install Advantage Kit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,16 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -14,12 +21,15 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   public static final boolean isCompetition = false;
 
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  // TODO : back log card to auto detecting
+  private final boolean IS_REPLAY = false;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -27,6 +37,32 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    Logger.recordMetadata("ProjectName", "6377_crescendo_2024");
+    Logger.recordMetadata("Repository", BuildConstants.MAVEN_NAME);
+    Logger.recordMetadata("Commit ID (GIT_SHA)", BuildConstants.GIT_SHA);
+    Logger.recordMetadata("Branch Name", BuildConstants.GIT_BRANCH);
+
+    if (isReal()) {
+      Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+      Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+      new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
+    } else if (IS_REPLAY) {
+      setUseTiming(false); // Run as fast as possible
+
+      String logPath =
+          LogFileUtil
+              .findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+      Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+      Logger.addDataReceiver(
+          new WPILOGWriter(
+              LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+    }
+
+    // Logger.disableDeterministicTimestamps() // See "Deterministic Timestamps" in the
+    // "Understanding Data Flow" page
+    Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may
+    // be added.
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
