2d5b8de9407608622633c4b3073f88f1aa3759fe
zaneb <zanebrotherton@gmail.com>
2024-01-16 20:48:43 -0600

    Rewrite to use reflection

--- src/main/java/frc/robot/config/DynamicRobotConfig.java
+++ src/main/java/frc/robot/config/DynamicRobotConfig.java
@@ -1,6 +1,8 @@
 package frc.robot.config;
 
 import edu.wpi.first.wpilibj.Preferences;
+import frc.robot.Robot;
+import java.lang.reflect.Field;
 import java.util.logging.Logger;
 
 /**
@@ -10,91 +12,56 @@ import java.util.logging.Logger;
  * IDs, Conversions, or setpoint information in this config.
  */
 public class DynamicRobotConfig {
-  private static DynamicRobotConfig dynRobotConfig;
-
-  // Keys to load values from, shall not be modified under any circumstance
-  private static final String frontLeftOffset_key = "front Left Offset";
-  private static final String frontRightOffset_key = "front Right Offset";
-  private static final String backRightOffset_key = "back Right Offset";
-  private static final String backLeftOffset_key = "back Left Offset";
-
-  // Contains every key that is used
-  // if one is missing it won't be correctly saved, or init
-  private static final String[] allKeys = {
-    frontLeftOffset_key, frontRightOffset_key, backRightOffset_key, backLeftOffset_key
-  };
-
-  public final double frontLeftOffset;
-  public final double frontRightOffset;
-  public final double backRightOffset;
-  public final double backLeftOffset;
-
-  public TunerConstants getTunerConstants() {
-    return new TunerConstants(frontLeftOffset, frontRightOffset, backLeftOffset, backRightOffset);
+  private static class ConfigVariables {
+    public static double frontLeftOffset = 0;
+    public static double frontRightOffset = 0;
+    public static double backLeftOffset = 2;
+    public static double backRightOffset = 0;
   }
+
+  private static Logger logger = Logger.getLogger(DynamicRobotConfig.class.getName());
 
   private DynamicRobotConfig() {
-    boolean initNT = false;
-
-    if (Preferences.containsKey(frontLeftOffset_key)) {
-      frontLeftOffset = Preferences.getDouble(frontLeftOffset_key, 0d);
-    } else {
-      raiseWarning("Front Left Pod Offset NOT FOUND!! using default");
-      frontLeftOffset = 0;
-      initNT = true;
-    }
-
-    if (Preferences.containsKey(frontRightOffset_key)) {
-      frontRightOffset = Preferences.getDouble(frontRightOffset_key, 0d);
-    } else {
-      raiseWarning("Front Right Pod Offset NOT FOUND!! using default");
-      frontRightOffset = 0;
-      initNT = true;
-    }
-
-    if (Preferences.containsKey(backRightOffset_key)) {
-      backRightOffset = Preferences.getDouble(backRightOffset_key, 0d);
-    } else {
-      raiseWarning("Back Right Pod Offset NOT FOUND!! using default");
-      backRightOffset = 0;
-      initNT = true;
-    }
-
-    if (Preferences.containsKey(backLeftOffset_key)) {
-      backLeftOffset = Preferences.getDouble(backLeftOffset_key, 0d);
-    } else {
-      raiseWarning("Back Left Pod Offset NOT FOUND!! using default");
-      backLeftOffset = 0;
-      initNT = true;
-    }
-
-    if (initNT) {
-      initNT();
+    // This code interacts with ConfigVariables using reflection, essentially using it as a map.
+    // First, the code gets a reference to every variable from the static class, referred to as a
+    // "Field"
+    // It then iterates through this list of references
+    for (Field variable : ConfigVariables.class.getFields()) {
+      // For each field, its name is stored as a string, so it can be used as a preference key
+      String key = variable.getName();
+      try {
+        // If the preference does not exist, or we are bypassing to competition robot defaults, log
+        // an alert and open network access for that preference
+        if (!Preferences.containsKey(key) || Robot.isCompetition) {
+          raiseWarning("Using competition robot default for " + key);
+          Preferences.initDouble(key, variable.getDouble(ConfigVariables.class));
+        }
+        // Set the field to the value pulled from preferences. If the preference encounters another
+        // error, use the field's startup value.
+        else {
+          variable.setDouble(
+              ConfigVariables.class,
+              Preferences.getDouble(key, variable.getDouble(ConfigVariables.class)));
+        }
+      } catch (IllegalAccessException e) {
+        raiseWarning("Invalid field " + key);
+      }
     }
   }
 
-  /** Creates network table entries even if not pre-existing */
-  public static void initNT() {
-    logInfo("Initlizing Dynamic Logs");
-    for (String key : allKeys) {
-      Preferences.initDouble(key, 0);
-    }
+  public TunerConstants getTunerConstants() {
+    return new TunerConstants(
+        ConfigVariables.frontLeftOffset,
+        ConfigVariables.frontRightOffset,
+        ConfigVariables.backLeftOffset,
+        ConfigVariables.backRightOffset);
   }
 
   public static DynamicRobotConfig loadDynamicRobotConfig() {
-    if (dynRobotConfig != null) {
-      return dynRobotConfig;
-    }
     return new DynamicRobotConfig();
   }
 
-  private static Logger DRC_logger = Logger.getLogger(DynamicRobotConfig.class.getName());
-
   private static void raiseWarning(String warning) {
-    DRC_logger.warning(warning);
-  }
-
-  private static void logInfo(String info) {
-    DRC_logger.info(info);
+    logger.warning(warning);
   }
 }
