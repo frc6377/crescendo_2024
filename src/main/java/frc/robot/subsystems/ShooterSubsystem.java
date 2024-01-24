bc420086647d2ea7490b2bc9cb4c8a23834179f5
aetherstorm-dev <50374209+aetherstorm-dev@users.noreply.github.com>
2024-01-23 20:04:11 -0600

    Command factory

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,10 +2,12 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
-
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 
-public class ShooterSubsystem {
+public class ShooterSubsystem extends SubsystemBase {
     private CANSparkMax shooterMotor;
     
     public ShooterSubsystem() {
@@ -14,8 +16,22 @@ public class ShooterSubsystem {
         shooterMotor.setSmartCurrentLimit(40);
     }
 
-    // Fold in speed calculation?
+    public Command shooterCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+        return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+    }
+
     public void setShooterSpeed(double speed) {
         shooterMotor.set(speed);
+    }
+
+    // Parameter: TargetType target ???
+    public double calculateShooterSpeed(double distance) {
+        double speed = distance; // Placeholder
+        return speed;
     }
 }
