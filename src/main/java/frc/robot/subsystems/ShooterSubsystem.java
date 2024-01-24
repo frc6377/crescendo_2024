2cd3de34bf285aa5b39faa4c59cc4402ca3299df
aetherstorm-dev <50374209+aetherstorm-dev@users.noreply.github.com>
2024-01-23 21:05:01 -0600

    Spotless

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,50 +2,50 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-    private CANSparkMax shooterMotor;
-    boolean readyToFire = true; // Take in readiness state from ShooterTrigger?
-    
-    public ShooterSubsystem() {
-        shooterMotor = new CANSparkMax(Constants.ShooterConstants.SHOOTER_MOTOR_ID, MotorType.kBrushless);
-        shooterMotor.restoreFactoryDefaults();
-        shooterMotor.setSmartCurrentLimit(40);
-    }
+  private CANSparkMax shooterMotor;
+  boolean readyToFire = true; // Take in readiness state from ShooterTrigger?
 
-    public Command shooterFire() {
-        return startEnd(
+  public ShooterSubsystem() {
+    shooterMotor =
+        new CANSparkMax(Constants.ShooterConstants.SHOOTER_MOTOR_ID, MotorType.kBrushless);
+    shooterMotor.restoreFactoryDefaults();
+    shooterMotor.setSmartCurrentLimit(40);
+  }
+
+  public Command shooterFire() {
+    return startEnd(
         () -> {
-            if (readyToFire) {
-                readyShooter();
-            }
+          if (readyToFire) {
+            readyShooter();
+          }
         },
         () -> {
-            setShooterSpeed(0);
+          setShooterSpeed(0);
         });
-    }
+  }
 
-    public void readyShooter() {
-        double speed = 0;
-        double distance = 0; // Take in distance from vision
-        // TargetType target =  ||| Take in target type from vision / elsewhere
+  public void readyShooter() {
+    double speed = 0;
+    double distance = 0; // Take in distance from vision
+    // TargetType target =  ||| Take in target type from vision / elsewhere
 
-        speed = calculateShooterSpeed(distance); // Include target
-        setShooterSpeed(speed);
-    }
+    speed = calculateShooterSpeed(distance); // Include target
+    setShooterSpeed(speed);
+  }
 
-    public void setShooterSpeed(double speed) {
-        shooterMotor.set(speed);
-    }
+  public void setShooterSpeed(double speed) {
+    shooterMotor.set(speed);
+  }
 
-    // Parameter: TargetType target ???
-    public double calculateShooterSpeed(double distance) {
-        double speed = distance; // Placeholder
-        return speed;
-    }
+  // Parameter: TargetType target ???
+  public double calculateShooterSpeed(double distance) {
+    double speed = distance; // Placeholder
+    return speed;
+  }
 }
