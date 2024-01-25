7bff750248b7d851a5be955c100701de60d94c26
zaneb <zanebrotherton@gmail.com>
2024-01-24 20:36:20 -0600

    Remove field code

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -20,8 +20,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.OI;
@@ -40,8 +38,6 @@ public class SwerveSubsystem extends SwerveDrivetrain implements Subsystem {
       Math.PI; // Max angular velocity in radians per second
   private final double drivetrainRadius;
   private final Telemetry telemetry = new Telemetry(maxSpeed);
-
-  private final Field2d field = new Field2d();
 
   private static boolean isFieldOriented = true;
   private static Rotation2d alignmentRotation = null;
@@ -86,7 +82,6 @@ public class SwerveSubsystem extends SwerveDrivetrain implements Subsystem {
                 && DriverStation.getAlliance().get() == Alliance.Red,
         this);
     this.registerTelemetry(telemetry::telemeterize);
-    SmartDashboard.putData("Field", field);
   }
 
   public SwerveSubsystem(
@@ -155,10 +150,6 @@ public class SwerveSubsystem extends SwerveDrivetrain implements Subsystem {
 
   public BiConsumer<Pose2d, Double> getVisionMeasurementConsumer() {
     return (t, u) -> addVisionMeasurement(t, u);
-  }
-
-  public void periodic() {
-    field.setRobotPose(super.m_odometry.getEstimatedPosition());
   }
 
   public SwerveRequest getBrakeRequest() {
