aadb0b3761be491492bfd86e13c952cbc9febf71
Adam Vander Pas <azvanderpas@gmail.com>
2024-01-24 21:27:38 -0600

    Add some more functionality to CANSparkMaxSim

--- src/main/java/frc/robot/CANSparkMaxSim.java
+++ src/main/java/frc/robot/CANSparkMaxSim.java
@@ -13,31 +13,55 @@ public class CANSparkMaxSim {
 
   public static final double kPeriod = 0.001; // 1kHz
   private SparkPIDController ctrl;
-  private double prevError, IState, setpoint, measurement;
+  private double prevError, IState, setpoint, position, velocity, output;
+  private boolean stopped;
   private ControlType type;
 
-  public CANSparkMaxSim(CANSparkMax spark, ControlType type) {
+  public CANSparkMaxSim(CANSparkMax spark) {
     ctrl = spark.getPIDController();
     setpoint = 0;
-    measurement = 0;
-    this.type = type;
+    position = 0;
+    velocity = 0;
+    this.type = ControlType.kVelocity;
     IState = 0;
+    output = 0;
+    stopped = true;
   }
 
-  public void setSetpoint(double setpoint) {
+  public void stopMotor() {
+    stopped = true;
+    output = 0;
+    IState = 0;
+    prevError = 0;
+  }
+
+  public void setSetpoint(double setpoint, ControlType t) {
+    stopped = false;
+    this.type = t;
     this.setpoint = setpoint;
   }
 
   public void update(double radsPerSec) {
-    if (type == ControlType.kVelocity) {
-      measurement = radsPerSec / (2 * Math.PI) * 60; // RPM
-    } else if (type == ControlType.kPosition) {
-      measurement += radsPerSec / (2 * Math.PI) * kPeriod;
-    }
+    velocity = radsPerSec / (2 * Math.PI) * 60; // RPM
+    position += radsPerSec / (2 * Math.PI) * kPeriod;
+    calculate();
+  }
+
+  public double getOutput() {
+    return output;
   }
 
   // Algorithm pulled from https://docs.revrobotics.com/sparkmax/operating-modes/closed-loop-control
-  public double get() {
+  private double calculate() {
+    if (stopped) {
+      return 0;
+    }
+    double measurement = 0;
+    if (type == ControlType.kVelocity) {
+      measurement = velocity;
+    } else if (type == ControlType.kPosition) {
+      measurement = position;
+    }
     double error = setpoint - measurement;
     double P_Err = error * ctrl.getP();
     if (Math.abs(error) <= ctrl.getIZone() || ctrl.getIZone() == 0d) {
@@ -50,7 +74,7 @@ public class CANSparkMaxSim {
     prevError = error;
     D_err *= ctrl.getD();
     double F_err = setpoint * ctrl.getFF();
-    double output = P_Err + IState + D_err + F_err;
+    output = P_Err + IState + D_err + F_err;
     output = Math.min(Math.max(output, ctrl.getOutputMin()), ctrl.getOutputMax());
     return output;
   }
