5e10cc41086f2d5a7da3fcc3c887e93354cc782b
Zane Brotherton <46907447+zbrotherton@users.noreply.github.com>
2024-01-25 17:31:54 -0800

    Check if action is null before putting control to dashboard (#90)

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -48,13 +48,13 @@ public class OI {
     public static final Control RSB =
         new Control(XboxController.Button.kRightStick, null, controller);
 
-    // Controll Curves
+    // Control Curves
     private static final ControlCurve xTranslationCurve = new ControlCurve(1, 0, 0, 0.0);
     private static final ControlCurve yTranslationCurve = new ControlCurve(1, 0, 0, 0.0);
     public static final ControlCurve translationMagnitudeCurve = new ControlCurve(1, 0, 1, 0.1);
     private static final ControlCurve rotationCurve = new ControlCurve(0.8, 0, 1, 0.1, true);
 
-    // Joystick Axies
+    // Joystick Axes
     public static final Control xTranslationAxis =
         new Control(XboxController.Axis.kLeftX, "X Translation", controller, xTranslationCurve);
     public static final Control yTranslationAxis =
@@ -96,13 +96,13 @@ public class OI {
     public static final Control RSB =
         new Control(XboxController.Button.kRightStick, null, controller);
 
-    // Controll Curves
+    // Control Curves
     private static final ControlCurve xTranslationCurve = new ControlCurve(1, 0, 0, 0.0, true);
     private static final ControlCurve yTranslationCurve = new ControlCurve(1, 0, 0, 0.0);
     public static final ControlCurve translationMagnitudeCurve = new ControlCurve(1, 0, 1, 0.1);
     private static final ControlCurve rotationCurve = new ControlCurve(0.8, 0, 1, 0.1, true);
 
-    // Joystick Axies
+    // Joystick Axes
     public static final Control xTranslationAxis =
         new Control(XboxController.Axis.kLeftX, "X Translation", controller, xTranslationCurve);
     public static final Control yTranslationAxis =
@@ -229,15 +229,17 @@ public class OI {
     }
 
     private void putControl() {
-      if (controller.getPort() == driverJoystickPort) {
-        driverControlsLayout.add(
-            "Driver " + getType().toString() + " " + String.valueOf(getId()),
-            type.toString() + " " + getName() + ": " + getAction());
-      }
-      if (controller.getPort() == operatorJoystickPort) {
-        operatorControlsLayout.add(
-            "Operator " + getType().toString() + " " + String.valueOf(getId()),
-            type.toString() + " " + getName() + ": " + getAction());
+      if (getAction() != null) {
+        if (controller.getPort() == driverJoystickPort) {
+          driverControlsLayout.add(
+              "Driver " + getType().toString() + " " + String.valueOf(getId()),
+              type.toString() + " " + getName() + ": " + getAction());
+        }
+        if (controller.getPort() == operatorJoystickPort) {
+          operatorControlsLayout.add(
+              "Operator " + getType().toString() + " " + String.valueOf(getId()),
+              type.toString() + " " + getName() + ": " + getAction());
+        }
       }
     }
   }
