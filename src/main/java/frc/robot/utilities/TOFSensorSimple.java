98727c349f7b9ab30a2cddb5b4a0c5ee39d1bbc4
riversry <79735432+ModBoyex@users.noreply.github.com>
2024-02-08 19:22:17 -0600

    Fix Sintax, Naming, and function use

--- src/main/java/frc/robot/utilities/TOFSensorSimple.java
+++ src/main/java/frc/robot/utilities/TOFSensorSimple.java
@@ -5,38 +5,32 @@
 package frc.robot.utilities;
 
 import com.playingwithfusion.TimeOfFlight;
-import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import java.util.function.BooleanSupplier;
 
 /** Add your docs here. */
 public class TOFSensorSimple {
-  private TimeOfFlight Sensor;
+  private TimeOfFlight sensor;
   private double threshold;
 
   public TOFSensorSimple(int ID, double threshold) {
-    Sensor = new TimeOfFlight(ID);
+    sensor = new TimeOfFlight(ID);
     this.threshold = threshold;
   }
 
   public double getDistance() {
-    return Sensor.getRange();
+    return sensor.getRange();
   }
 
-  public BooleanSupplier isBeamBroke() {
-    return () -> (getDistance() < threshold);
+  public boolean isBeamBroke() {
+    return getDistance() < threshold;
   }
 
-  public Trigger getBeamTrigger(Command action) {
-    return new Trigger(isBeamBroke());
+  public Trigger beamBroken(Command action) {
+    return new Trigger(this::isBeamBroke);
   }
 
-  public void identify() {
-    Sensor.identifySensor();
-  }
-
-  public void putBuilder(SendableBuilder builder) {
-    Sensor.initSendable(builder);
+  public void blink() {
+    sensor.identifySensor();
   }
 }
