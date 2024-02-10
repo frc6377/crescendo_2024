4f0645fd844d406fe38b9fa3579bc03b5a682ba1
ModBoyex <79735432+ModBoyex@users.noreply.github.com>
2024-02-10 11:50:48 -0600

    Tof sensors (#125)
    <!-- GH PRs use Markdown formatting, see
    https://www.markdownguide.org/basic-syntax/ -->
    
    ## Justification
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If this PR fully addresses an issue, please say "Fixes #1234", -->
    <!--- as this will allow Github to automatically close the related Issue
    -->
    We want to be able to simply use the TOF sensor as a TOF or Beam Break
    sensor
    
    ## Implementaion
    <!--- Explain what was done to address the problem/need -->
    <!--- Also mention any known or possible side effects -->
    Creates a class for the sensor that has getters that can work for a TOF
    or Beam Break
    
    ## Testing Done
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you -->
    <!--- ran to see how your change affects other areas of the code, -->
    <!--- etc. Then, include justifications for how your tests -->
    <!--- demonstrate those affects. -->
    N/A
    
    ---------
    
    Co-authored-by: azvanderpas <azvanderpas@gmail.com>

---
+++ src/main/java/frc/robot/utilities/TOFSensorSimple.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utilities;
+
+import com.playingwithfusion.TimeOfFlight;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/** Add your docs here. */
+public class TOFSensorSimple {
+  private TimeOfFlight sensor;
+  private double threshold;
+
+  public TOFSensorSimple(int ID, double threshold) {
+    sensor = new TimeOfFlight(ID);
+    this.threshold = threshold; // in mm
+  }
+
+  public double getMilliMeters() {
+    return sensor.getRange();
+  }
+
+  public boolean isBeamBroke() {
+    return getMilliMeters() < threshold;
+  }
+
+  public Trigger beamBroken(Command action) {
+    return new Trigger(this::isBeamBroke);
+  }
+
+  public void blink() {
+    sensor.identifySensor();
+  }
+}
