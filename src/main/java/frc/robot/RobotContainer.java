2a9183e04a45b86d094561535bf4230ba42e1af6
Josh-Westbrook <116318801+Josh-Westbrook@users.noreply.github.com>
2024-02-10 16:50:27 -0600

    Make turret disableable (#135)
    <!-- GH PRs use Markdown formatting, see
    https://www.markdownguide.org/basic-syntax/ -->
    
    ## Justification
    <!--- Why is this change required? What problem does it solve? -->
    Turret was not part of the partial bot/disableable subsystems code. Time
    to change that!
    <!--- If this PR fully addresses an issue, please say "Fixes #1234", -->
    <!--- as this will allow Github to automatically close the related Issue
    -->

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -51,7 +51,7 @@ public class RobotContainer {
   private final IntakeSubsystem intakeSubsystem;
   private final ShooterSubsystem shooterSubsystem;
   private final TriggerSubsystem triggerSubsystem;
-  private final TurretSubsystem turretSubsystem = new TurretSubsystem(robotStateManager);
+  private final TurretSubsystem turretSubsystem;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final SwerveSubsystem drivetrain;
@@ -74,6 +74,11 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    if (Constants.enabledSubsystems.turretEnabled) {
+      turretSubsystem = new TurretSubsystem(robotStateManager);
+    } else {
+      turretSubsystem = null;
+    }
     if (Constants.enabledSubsystems.shooterEnabled) {
       shooterSubsystem = new ShooterSubsystem();
     } else {
@@ -96,7 +101,8 @@ public class RobotContainer {
       intakeSubsystem = null;
     }
     triggerSubsystem = new TriggerSubsystem();
-    if (Constants.enabledSubsystems.limeLightEnabled) {
+    if (Constants.enabledSubsystems.limeLightEnabled
+        && Constants.enabledSubsystems.drivetrainEnabled) {
       limelightSubsystem = new LimelightSubsystem(drivetrain.getVisionMeasurementConsumer());
     } else {
       limelightSubsystem = null;
@@ -107,11 +113,13 @@ public class RobotContainer {
       trapElvSubsystem = null;
     }
     // Configure the trigger bindings
-    configureBindings();
-    registerCommands();
-    autoChooser = AutoBuilder.buildAutoChooser();
-    configTab.add("Auton Selection", autoChooser).withSize(3, 1);
-    SmartDashboard.putBoolean("NamedCommand test", false);
+    if (Constants.enabledSubsystems.drivetrainEnabled) {
+      configureBindings();
+      registerCommands();
+      autoChooser = AutoBuilder.buildAutoChooser();
+      configTab.add("Auton Selection", autoChooser).withSize(3, 1);
+      SmartDashboard.putBoolean("NamedCommand test", false);
+    }
   }
 
   /**
@@ -178,9 +186,10 @@ public class RobotContainer {
       OI.getTrigger(OI.Operator.shooterTrigger).onTrue(shooterSubsystem.shooterFire());
     }
     // Turret commands
-    turretSubsystem.setDefaultCommand(turretSubsystem.idleTurret());
-    OI.getTrigger(OI.Operator.B).toggleOnTrue(turretSubsystem.getAimTurretCommand());
-
+    if (Constants.enabledSubsystems.turretEnabled) {
+      turretSubsystem.setDefaultCommand(turretSubsystem.idleTurret());
+      OI.getTrigger(OI.Operator.B).toggleOnTrue(turretSubsystem.getAimTurretCommand());
+    }
     // Trap Elv Intaking
     if (Constants.enabledSubsystems.elvEnabled) {
       OI.getButton(OI.Driver.groundIntakeButton)
@@ -238,8 +247,11 @@ public class RobotContainer {
    * @return the command to run in autonomous(including the delay)
    */
   public Command getAutonomousCommand() {
-    return new WaitCommand(autoDelay.getDouble(0))
-        .andThen(autoChooser.getSelected())
-        .withName("Get Auto Command");
+    if (Constants.enabledSubsystems.drivetrainEnabled) {
+      return new WaitCommand(autoDelay.getDouble(0))
+          .andThen(autoChooser.getSelected())
+          .withName("Get Auto Command");
+    }
+    return null;
   }
 }
