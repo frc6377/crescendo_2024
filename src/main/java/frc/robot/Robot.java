f7a7658785b4bc4dff79d44aa474c0740cb5d115
Josh Westbrook <joshwestbrook2008@gmail.com>
2024-02-10 14:27:13 -0600

    Log commands are being run

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import java.io.ByteArrayInputStream;
@@ -142,7 +143,11 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    if (!isCompetition){
+      SmartDashboard.putData(CommandScheduler.getInstance());
+    }
+  }
 
   @Override
   public void teleopExit() {}
