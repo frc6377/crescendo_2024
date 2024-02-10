709a1a8618e8dd93de678a4a8f3ea4e8aa57bb73
malachil <mdl@jdlmx.com>
2024-02-10 14:41:51 -0600

    limelight distance to tag test

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -6,8 +6,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -21,8 +19,10 @@ public class LimelightSubsystem extends SubsystemBase {
 
   private int measurementsUsed = 0;
   private DebugEntry<Integer> measurementEntry = new DebugEntry<Integer>(0, "measurements", this);
-  private DebugEntry<Double> distanceEntryTag3 = new DebugEntry<Double>(0.0, "Tag 3 Distance (m)", this);
-  private DebugEntry<Double> distanceEntryTag4 = new DebugEntry<Double>(0.0, "Tag 4 Distance (m)", this);
+  private DebugEntry<Double> distanceEntryTag3 =
+      new DebugEntry<Double>(0.0, "Tag 3 Distance (m)", this);
+  private DebugEntry<Double> distanceEntryTag4 =
+      new DebugEntry<Double>(0.0, "Tag 4 Distance (m)", this);
 
   private int lastHeartbeat = 0;
 
@@ -42,12 +42,21 @@ public class LimelightSubsystem extends SubsystemBase {
 
   private Pose2d getPose2d() {
     Pose3d botpose = LimelightHelpers.getBotPose3d_wpiBlue("");
-    double distanceToTag3 = Math.sqrt(Math.pow(16.579342 - botpose.getX(), 2) + Math.pow(4.982718 - botpose.getY(), 2) + Math.pow(1.451102 - botpose.getZ(), 2));
-    double distanceToTag4 = Math.sqrt(Math.pow(16.579342 - botpose.getX(), 2) + Math.pow(5.547868 - botpose.getY(), 2) + Math.pow(1.451102 - botpose.getZ(), 2));
+    double distanceToTag3 =
+        Math.sqrt(
+            Math.pow(16.579342 - botpose.getX(), 2)
+                + Math.pow(4.982718 - botpose.getY(), 2)
+                + Math.pow(1.451102 - botpose.getZ(), 2));
+    double distanceToTag4 =
+        Math.sqrt(
+            Math.pow(16.579342 - botpose.getX(), 2)
+                + Math.pow(5.547868 - botpose.getY(), 2)
+                + Math.pow(1.451102 - botpose.getZ(), 2));
     distanceEntryTag3.log(distanceToTag3);
     distanceEntryTag4.log(distanceToTag4);
     return botpose.toPose2d();
-    //return new Pose2d(botpose[0], botpose[1], new Rotation2d(Units.degreesToRadians(botpose[5])));
+    // return new Pose2d(botpose[0], botpose[1], new
+    // Rotation2d(Units.degreesToRadians(botpose[5])));
   }
 
   private double getTime() {
