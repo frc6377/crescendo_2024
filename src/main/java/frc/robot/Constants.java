505e2592b01693bf749e511231e99f4735fc162a
riversry <79735432+ModBoyex@users.noreply.github.com>
2024-02-11 14:11:59 -0600

    Update number (exept MOI)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -72,11 +72,11 @@ public final class Constants {
     public static final double KMAXOUTPUT = 1;
     public static final double KMINOUTPUT = -1;
     public static final int MAX_TURRET_ANGLE_DEGREES = 110;
-    public static final double GEAR_RATIO = 6.8333; // 0.25 for the revbot prototype turret
+    public static final double GEAR_RATIO = 6.8333; // (24:164)  0.25 for the revbot prototype turret
 
     // Simulation Vals
     public static final double TURRET_MOI = 0; // Moment of Inertia
-    public static final double TURRET_RADIUS = 0;
+    public static final double TURRET_RADIUS = Units.inchesToMeters(12);
   }
 
   public static class OperatorConstants {
