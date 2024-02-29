0560a8650d46396a7f9f31e86da3a03f3e91aadb
KianChilds <94756266+KianChilds@users.noreply.github.com>
2024-02-28 22:30:57 -0600

    testing preload shoot path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -281,9 +280,10 @@ public class RobotContainer {
 
   private Command shootAuton() {
     return Commands.deadline(
-        Commands.waitUntil(() -> shooterSubsystem.isShooterReady())
-            .andThen(triggerCommandFactory.getShootCommand().withTimeout(2)),
-        shooterCommandFactory.revShooter());
+            Commands.waitUntil(() -> shooterSubsystem.isShooterReady())
+                .andThen(triggerCommandFactory.getShootCommand().withTimeout(5)),
+            shooterCommandFactory.revShooter())
+        .andThen(shooterCommandFactory.shooterIdle().withTimeout(.02));
   }
 
   private Command ampAuton() {
