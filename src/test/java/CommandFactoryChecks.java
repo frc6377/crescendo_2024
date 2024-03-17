72e7c201370347518d88be011e466b90c131f400
Adam Vander Pas <azvanderpas@gmail.com>
2024-03-17 11:58:56 -0500

    Rename ProxyCmdCheck to CommandFactoryChecks

--- src/test/java/CommandFactoryChecks.java
+++ src/test/java/CommandFactoryChecks.java
@@ -26,7 +26,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ProxyCmdCheck {
+public class CommandFactoryChecks {
 
   @BeforeEach
   public void setup() {}
