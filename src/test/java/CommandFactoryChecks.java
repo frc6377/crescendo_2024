ac1c64da38939aea41028502ef8341356ef6bc3f
Adam Vander Pas <azvanderpas@gmail.com>
2024-03-17 12:02:04 -0500

    Add some more descriptive error messages

--- src/test/java/CommandFactoryChecks.java
+++ src/test/java/CommandFactoryChecks.java
@@ -116,7 +116,7 @@ public class CommandFactoryChecks {
     for (int i = 0; i < cmds.length; i++) {
       String name = cmds[i].getName();
       for (int j = i + 1; j < cmds.length; j++) {
-        assertNotEquals(name, cmds[j].getName());
+        assertNotEquals(name, cmds[j].getName(), "Duplicate Command \"" + name + "\" in getCommands list");
       }
     }
   }
@@ -129,9 +129,9 @@ public class CommandFactoryChecks {
       }
     }
     noDuplicatesInGetCommands(cmds);
-    assertEquals(cmds.length, numCommands);
+    assertEquals(cmds.length, numCommands, "getCommands() list size mismatch with number of Command factory methods");
     for (Command cmd : cmds) {
-      assertEquals(new HashSet<Subsystem>(), cmd.getRequirements());
+      assertEquals(new HashSet<Subsystem>(), cmd.getRequirements(), "Non-proxied public Command factory detected");
     }
   }
 }
