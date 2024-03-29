6868198fa811b93b6dbd3eb2edc95e6077f4b3b0
riversry <104952320+riversry@users.noreply.github.com>
2024-03-28 21:29:43 -0500

    Create Subscribers

--- src/main/java/frc/robot/utilities/TunableNumber.java
+++ src/main/java/frc/robot/utilities/TunableNumber.java
@@ -1,9 +1,6 @@
 package frc.robot.utilities;
 
-import edu.wpi.first.networktables.DoubleSubscriber;
-import edu.wpi.first.networktables.DoubleTopic;
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -13,15 +10,10 @@ import java.util.function.Consumer;
 import java.util.function.DoubleSupplier;
 
 public class TunableNumber extends SubsystemBase implements DoubleSupplier {
-  private NetworkTableInstance inst;
   private static ShuffleboardTab tuningTab;
   private GenericEntry numberEntry;
   private double value;
-  private double defaultValue;
   private Consumer<Double> consumer;
-
-  private DoubleTopic doubleTopic;
-  private DoubleSubscriber doubleSub;
 
   public TunableNumber(String name, double defaultValue, Subsystem subsystem) {
     this(name, defaultValue, (ignored) -> {}, subsystem);
@@ -33,18 +25,19 @@ public class TunableNumber extends SubsystemBase implements DoubleSupplier {
     this.value = defaultValue;
     this.consumer = consumer;
     if (!Robot.isCompetition) {
-      inst = NetworkTableInstance.getDefault();
       numberEntry = tuningTab.add(name, defaultValue).getEntry();
-      doubleTopic = inst.getDoubleTopic(name);
-      doubleSub = doubleTopic.subscribe(defaultValue);
     }
   }
 
   public void periodic() {
     if (!Robot.isCompetition) {
-      consumer.accept(doubleSub.get(defaultValue));
-      value = doubleSub.get(defaultValue);
+      consumer.accept(numberEntry.getDouble(value));
+      value = numberEntry.getDouble(value);
     }
+  }
+
+  public double get() {
+    return value;
   }
 
   public double getAsDouble() {
