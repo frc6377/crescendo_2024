1dfd79d512187be27015f4ace0249a6fc7f003bd
KianChilds <94756266+KianChilds@users.noreply.github.com>
2024-03-30 11:58:20 -0500

    Feature/modes (#238)
    <!-- GH PRs use Markdown formatting, see
    https://www.markdownguide.org/basic-syntax/ -->
    
    ## Justification
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If this PR fully addresses an issue, please say "Fixes #1234", -->
    <!--- as this will allow Github to automatically close the related Issue
    -->
    
    ## Implementaion
    <!--- Explain what was done to address the problem/need -->
    <!--- Also mention any known or possible side effects -->
    
    ## Testing Done
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you -->
    <!--- ran to see how your change affects other areas of the code, -->
    <!--- etc. Then, include justifications for how your tests -->
    <!--- demonstrate those affects. -->
    
    ---------
    
    Co-authored-by: JD Lewis <dave.lewis@atxrobotics.org>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -25,6 +24,7 @@ import frc.robot.Constants.enabledSubsystems;
 import frc.robot.config.TunerConstants;
 import frc.robot.stateManagement.AllianceColor;
 import frc.robot.stateManagement.RobotStateManager;
+import frc.robot.stateManagement.ShooterMode;
 import frc.robot.subsystems.climberSubsystem.ClimberCommandFactory;
 import frc.robot.subsystems.climberSubsystem.ClimberSubsystem;
 import frc.robot.subsystems.intakeSubsystem.IntakeCommandFactory;
@@ -257,7 +257,10 @@ public class RobotContainer {
     new Trigger(() -> OI.Operator.controller.getPOV() == 0).whileTrue(intakeCommand());
     new Trigger(() -> OI.Operator.controller.getPOV() == 180).whileTrue(outtakeCommand());
     new Trigger(() -> OI.Operator.controller.getPOV() == 90)
-        .onTrue(new InstantCommand(() -> robotStateManager.setShortRange()));
+        .whileTrue(
+            robotStateManager.setShooterMode(ShooterMode.SHORT_RANGE, ShooterMode.LONG_RANGE));
+    new Trigger(() -> OI.Operator.controller.getPOV() == 270)
+        .whileTrue(robotStateManager.setShooterMode(ShooterMode.LOB, ShooterMode.LONG_RANGE));
 
     new Trigger(() -> OI.Driver.controller.getPOV() == 0).whileTrue(intakeCommand());
   }
