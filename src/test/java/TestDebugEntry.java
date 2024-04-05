432fef16fefc6d8f6b1abe8a4e9fec6b63d9c951
riversry <104952320+riversry@users.noreply.github.com>
2024-04-04 23:30:35 -0500

    Debug Entry Update

--- src/test/java/TestDebugEntry.java
+++ src/test/java/TestDebugEntry.java
@@ -1,6 +1,5 @@
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 import edu.wpi.first.hal.HAL;
@@ -33,47 +32,6 @@ public class TestDebugEntry {
     HAL.shutdown();
     subsystem = new TestSubsystem();
     subsystem2 = new TestSubsystem();
-  }
-
-  @Test()
-  public void raiseDupEntryError() {
-    // Duplicate Entry
-    if (Robot.isCompetition) {
-      return;
-    }
-    try (MockedStatic<DriverStation> mockedFactory = Mockito.mockStatic(DriverStation.class)) {
-      mockedFactory
-          .when(() -> DriverStation.reportError(anyString(), anyBoolean()))
-          .thenCallRealMethod();
-      new DebugEntry<Double>(0.0, "test", subsystem);
-      new DebugEntry<Double>(0.0, "test", subsystem);
-      mockedFactory.verify(() -> DriverStation.reportError(anyString(), anyBoolean()), times(1));
-    }
-  }
-
-  @Test
-  public void raiseDataTypeError() {
-    if (Robot.isCompetition) {
-      return;
-    }
-    try (MockedStatic<DriverStation> mockedFactory = Mockito.mockStatic(DriverStation.class)) {
-      mockedFactory
-          .when(() -> DriverStation.reportWarning(anyString(), anyBoolean()))
-          .thenCallRealMethod();
-
-      mockedFactory
-          .when(() -> DriverStation.reportError(anyString(), anyBoolean()))
-          .thenCallRealMethod();
-
-      DebugEntry<Float> dut =
-          new DebugEntry<Float>(Float.valueOf((float) 10.0), "test2", subsystem);
-      mockedFactory.verify(() -> DriverStation.reportWarning(anyString(), anyBoolean()), times(1));
-
-      dut.log((float) 10.0);
-      mockedFactory.verify(
-          () -> DriverStation.reportError(eq("Invalid type for log " + "test2"), anyBoolean()),
-          times(1));
-    }
   }
 
   @Test
